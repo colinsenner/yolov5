5c46936c453c8cf6934412a6042c28b5bbd53ac1
Colin Senner <colin.senner@gmail.com>
2020-06-23 20:40:47 +0000

    Update

--- check_tpus.py
+++ check_tpus.py
@@ -2,6 +2,9 @@ import torch
 import torch_xla
 import torch_xla.core.xla_model as xm
 
+print("Starting...")
+print("Supported xla devices")
+print(xm.get_xla_supported_devices())
 t = torch.randn(2, 2, device=xm.xla_device())
 print(t.device)
 print(t)
